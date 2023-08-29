ade14dd2a6e1cf9e26157b1c33873a076fd989ad
janalojzgacnik <gacnik.jan@gmail.com>
2023-08-29 16:45:36 +0200

    Initial commit

---
+++ src/main/java/com/resort/platform/backnode/BacknodeApplication.java
@@ -0,0 +1,13 @@
+package com.resort.platform.backnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacknodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacknodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resort/platform/backnode/BacknodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resort.platform.backnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacknodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
